342b47649ba4090a7fc288e6d1ebc2da45388842
Jonay Déniz <alu0101791623@ull.edu.es>
2026-02-03 19:24:13 +0000

    Create registros.s

---
+++ registros.s
@@ -0,0 +1,9 @@
+Convenio de registros:
+
+$v0 / $v1 -> Sirven para devolver valores de funciones
+
+$a0 - $a3 -> Argumentos de la función
+
+$t0 - $t9 -> Temporales (se borran con un syscall o llamadas a funciones)
+
+$s0 - $s8 -> Salvados (no se borran con un syscall o llamadas a funciones)
