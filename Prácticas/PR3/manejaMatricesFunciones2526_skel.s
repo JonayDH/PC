98ee5542c21eaf6a5a62e487aa23d2a0b4e2f485
Jonay <alu0101791623@ull.edu.es>
2026-04-11 10:27:19 +0100

    Update manejaMatricesFunciones2526_skel.s
    - Acabada función procesa_cols

--- Prácticas/PR3/manejaMatricesFunciones2526_skel.s
+++ Prácticas/PR3/manejaMatricesFunciones2526_skel.s
@@ -352,6 +352,137 @@ cadFin:         .asciiz "\n\nTermina el programa\n"
 	.text
 
 
+# void procesa_cols(structMat* mat, int indC1, int indC2) {
+procesa_cols:
+# Parámetros de entrada:
+# mat -> $a0 ->
+# indC1 -> $a1 -> $s6
+# indC2 -> $a2 -> $s7
+# Parámetros de salida: ninguno
+
+# tabla de registros:
+# numCol -> $s0
+# numFil -> $s1
+# datos -> $s2
+# fa -> $s3
+# e1 -> $s4
+# e2 -> $s5
+# val1 -> $f20
+# val2 -> $f22
+
+# Esta función necesita usar la pila
+# push: $ra, $s0, $s1, $s2, $s3, $s4, $s5, $f20, $f22, $s6, $s7
+	addi $sp,-52
+	sw $ra,0($sp)
+	sw $s0,4($sp)
+	sw $s1,8($sp)
+	sw $s2,12($sp)
+	sw $s3,16($sp)
+	sw $s4,20($sp)
+	sw $s5,24($sp)
+	s.d $f20,28($sp)
+	s.d $f22,36($sp)
+	sw $s6,44($sp)
+	sw $s7,48($sp)
+
+	move $s6,$a1
+	move $s7,$a2
+
+#   int numCol = mat->nCol;
+	lw $s0,nCol($a0)
+
+#   int numFil = mat->nFil;
+	lw $s1,nFil($a0)
+
+#   double* datos = mat->elementos;
+	la $s2,elementos($a0)
+
+#   for(int fa = 0; fa < numFil; fa++) {
+	li $s3,0
+	procesa_cols_for_condicion:
+
+		blt $s3,$s1,procesa_cols_for_dentro
+		b procesa_cols_for_fuera
+
+	procesa_cols_for_dentro:
+
+#     // e1 = &(datos[fa][indC1]);
+#     double* e1 = datos + (fa * numCol + indC1);
+		mul $s4,$s3,$s0
+		add $s4,$s4,$s6
+		mul $s4,$s4,tamD
+		add $s4,$s4,$s2
+
+#     // e2 = &(datos[fa][indC2]);
+#     double* e2 = datos + (fa * numCol + indC2);
+		mul $s5,$s3,$s0
+		add $s5,$s5,$s7
+		mul $s5,$s5,tamD
+		add $s5,$s5,$s2
+
+#     double val1 = *e1;
+		l.d $f20,0($s4)
+
+#     double val2 = *e2;
+		l.d $f22,0($s5)
+
+#     if(val1 > val2) {
+		procesa_cols_if_condicion:
+
+			c.le.d $f20,$f22
+			bc1f procesa_cols_if_dentro
+			b procesa_cols_if_else
+
+		procesa_cols_if_dentro:
+
+#       *e1 = val1 / 2.0;
+			li.d $f4,2.0
+			div.d $f4,$f20,$f4
+			s.d $f4,0($s4)
+
+			b procesa_cols_if_fin
+#     } else {
+		procesa_cols_if_else:
+
+#       swap(e1, e2);
+			move $a0,$s4
+			move $a1,$s5
+
+			jal swap
+#     }
+		procesa_cols_if_fin:
+
+#     *e2 = *e2 + 0.5625;
+		li.d $f6,0.5625
+		l.d $f8,0($s5)
+		add.d $f6,$f8,$f6
+		s.d $f6,0($s5)
+
+		addi $s3,1
+		b procesa_cols_for_condicion
+
+	procesa_cols_for_fuera:
+#   }
+# }
+
+# pop:
+	lw $ra,0($sp)
+	lw $s0,4($sp)
+	lw $s1,8($sp)
+	lw $s2,12($sp)
+	lw $s3,16($sp)
+	lw $s4,20($sp)
+	lw $s5,24($sp)
+	l.d $f20,28($sp)
+	l.d $f22,36($sp)
+	lw $s6,44($sp)
+	lw $s7,48($sp)
+	addi $sp,52
+
+	jr $ra
+
+procesa_cols__MARCAFIN:
+
 
 
 # void print_mat(structMat* mat) {
